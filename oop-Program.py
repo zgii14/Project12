44cb3d7bf8bf2ed2f8a6670d06bcf1de85741051
zgii14 <129758748+zgii14@users.noreply.github.com>
2023-04-03 21:45:11 +0700

    Update oop-Program.py

--- oop-Program.py
+++ oop-Program.py
@@ -1,16 +1,24 @@
-class Manusia:
-    # inisialisasi kelas Manusia
-    def __init__(self, nama, umur):
+class Karyawan:
+    jumlah_karyawan = 0
+
+    def __init__(self, nama, gaji):
         self.nama = nama
-        self.umur = umur
+        self.gaji = gaji
+        Karyawan.jumlah_karyawan += 1
 
-    # metode untuk mengatakan halo
-    def sapa(self):
-        print(f'Halo, nama saya {self.nama} dan saya berumur {self.umur} tahun')
+    def tampilkan_jumlah(self):
+        print("Total karyawan:", Karyawan.jumlah_karyawan)
 
-# membuat objek dari kelas Manusia
-saya = Manusia('John', 30)
+    def tampilkan_profil(self):
+        print("Nama :", self.nama)
+        print("Gaji :", self.gaji)
+        print()
 
-# memanggil metode sapa()
-saya.sapa()
+# Membuat objek pertama dari kelas Karyawan
+karyawan1 = Karyawan("Sarah", 1000000)
+# Membuat objek kedua dari kelas Karyawan
+karyawan2 = Karyawan("Budi", 2000000)
 
+karyawan1.tampilkan_profil()
+karyawan2.tampilkan_profil()
+print("Total karyawan :", Karyawan.jumlah_karyawan)
